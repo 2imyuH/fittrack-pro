63467493983a69a2e79ebbba12284c7f0d2446f9
HuyIsMe <99466091+2imyuH@users.noreply.github.com>
2025-10-13 08:54:56 +0000

    Update features

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; // 👈 BẮT BUỘC
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 👈 để Tailwind quét các class trong thư mục src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
