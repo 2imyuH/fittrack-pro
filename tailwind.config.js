af27a989eb9c23591479db386a6028ba8f79f0b4
HuyIsMe <99466091+2imyuH@users.noreply.github.com>
2025-10-13 09:01:31 +0000

    Update features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // 👈 để Tailwind quét các class trong thư mục src
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
